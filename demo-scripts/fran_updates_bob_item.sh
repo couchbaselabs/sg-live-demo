b8bdf9ba3a6a17bb279294488fbc4a3c33a49e21
Andrew Reslan <andrew@couchbase.com>
2015-06-04 10:13:02 -0700

    Final presentation versions

--- demo-scripts/fran_updates_bob_item.sh
+++ demo-scripts/fran_updates_bob_item.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X PUT http://fran:password@127.0.0.1:4984/grocery-sync/521bfa55bb6b79dab98d22fb29b639d6 -H "Content-Type: application/json" -d '{"_rev":"1-fee07d799262aceb169f5244f6babad0", "type": "item","owner": "bob","text": "Low Fat Ice Cream","created_at":"2015-06-03T14:10:36.746Z","check": false}'
+curl -X PUT http://fran:password@127.0.0.1:4984/grocery-sync/40e004249ed116426158aa70020080aa -H "Content-Type: application/json" -d '{"_rev":"1-fee07d799262aceb169f5244f6babad0", "type": "item","owner": "bob","text": "Low Fat Ice Cream","created_at":"2015-06-03T14:10:36.746Z","check": false}'
