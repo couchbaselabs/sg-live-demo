b26e19144416df39be8ca8a3159cb3b2bff47dc9
Andrew Reslan <andrew@couchbase.com>
2015-09-10 17:53:48 +0100

    Updated SG config for demo 3 with correct form of throws method

--- data-load-scripts/data-load-0.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -X POST http://127.0.0.1:4984/grocery-sync/ -H "Content-Type: application/json" -d '{"text": "Chocolate","created_at":"2015-06-03T14:03:35.746Z","check": false}'
-curl -X POST http://127.0.0.1:4984/grocery-sync/ -H "Content-Type: application/json" -d '{"text": "Ice Cream","created_at":"2015-06-03T14:03:36.746Z","check": false}'
-curl -X POST http://127.0.0.1:4984/grocery-sync/ -H "Content-Type: application/json" -d '{"text": "Doughnuts","created_at":"2015-06-03T14:03:37.746Z","check": false}'
-curl -X POST http://127.0.0.1:4984/grocery-sync/ -H "Content-Type: application/json" -d '{"text": "Cakes","created_at":"2015-06-03T14:03:38.746Z","check": false}'
-curl -X POST http://127.0.0.1:4984/grocery-sync/ -H "Content-Type: application/json" -d '{"text": "Peptic Meds","created_at":"2015-06-03T14:03:39.746Z","check": false}'
