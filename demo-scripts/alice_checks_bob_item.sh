a45e90ef703f723375e9f90e2d6c8e9a38ce5a10
Andrew Reslan <andrew@couchbase.com>
2015-09-25 20:43:21 +0100

    Refactored to use fixed document names for data load items in second demo, this avoids having to manually update following modify scripts.

--- demo-scripts/alice_checks_bob_item.sh
+++ demo-scripts/alice_checks_bob_item.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X PUT http://alice:password@127.0.0.1:4984/grocery-sync/521bfa55bb6b79dab98d22fb29b639d6 -H "Content-Type: application/json" -d '{"_rev":"2-36fafd4b0bf57458f3da71b3d5cbc511", "type": "item","owner": "bob","text": "Low Fat Ice Cream","created_at":"2015-06-03T14:10:36.746Z","check": true}'
+curl -X PUT http://alice:password@127.0.0.1:4984/grocery-sync/bob-icecream -H "Content-Type: application/json" -d '{"_rev":"2-36fafd4b0bf57458f3da71b3d5cbc511", "type": "item","owner": "bob","text": "Low Fat Ice Cream","created_at":"2015-06-03T14:10:36.746Z","check": true}'
--- demo-scripts/data-load-2.sh
+++ demo-scripts/data-load-2.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-curl -X POST http://bob:password@127.0.0.1:4984/grocery-sync/ -H "Content-Type: application/json" -d '{"type": "item","owner": "bob","text": "Chocolate","created_at":"2015-06-03T14:10:35.746Z","check": false}'
-curl -X POST http://bob:password@127.0.0.1:4984/grocery-sync/ -H "Content-Type: application/json" -d '{"type": "item","owner": "bob","text": "Ice Cream","created_at":"2015-06-03T14:10:36.746Z","check": false}'
-curl -X POST http://bob:password@127.0.0.1:4984/grocery-sync/ -H "Content-Type: application/json" -d '{"type": "item","owner": "bob","text": "Doughnuts","created_at":"2015-06-03T14:10:37.746Z","check": false}'
-curl -X POST http://bob:password@127.0.0.1:4984/grocery-sync/ -H "Content-Type: application/json" -d '{"type": "item","owner": "bob","text": "Cakes","created_at":"2015-06-03T14:10:38.746Z","check": false}'
-curl -X POST http://bob:password@127.0.0.1:4984/grocery-sync/ -H "Content-Type: application/json" -d '{"type": "item","owner": "bob","text": "Peptic Meds","created_at":"2015-06-03T14:10:39.746Z","check": false}'
-curl -X POST http://alice:password@127.0.0.1:4984/grocery-sync/ -H "Content-Type: application/json" -d '{"type": "item","owner": "alice","text": "Carrots","created_at":"2015-06-03T14:10:40.746Z","check": false}'
-curl -X POST http://alice:password@127.0.0.1:4984/grocery-sync/ -H "Content-Type: application/json" -d '{"type": "item","owner": "alice","text": "Greens","created_at":"2015-06-03T14:10:41.746Z","check": false}'
-curl -X POST http://alice:password@127.0.0.1:4984/grocery-sync/ -H "Content-Type: application/json" -d '{"type": "item","owner": "alice","text": "Blueberries","created_at":"2015-06-03T14:10:42.746Z","check": false}'
+curl -X PUT http://bob:password@127.0.0.1:4984/grocery-sync/bob-chocolate -H "Content-Type: application/json" -d '{"type": "item","owner": "bob","text": "Chocolate","created_at":"2015-06-03T14:10:35.746Z","check": false}'
+curl -X PUT http://bob:password@127.0.0.1:4984/grocery-sync/bob-icecream -H "Content-Type: application/json" -d '{"type": "item","owner": "bob","text": "Ice Cream","created_at":"2015-06-03T14:10:36.746Z","check": false}'
+curl -X PUT http://bob:password@127.0.0.1:4984/grocery-sync/bob-doughnuts -H "Content-Type: application/json" -d '{"type": "item","owner": "bob","text": "Doughnuts","created_at":"2015-06-03T14:10:37.746Z","check": false}'
+curl -X PUT http://bob:password@127.0.0.1:4984/grocery-sync/bob-cakes -H "Content-Type: application/json" -d '{"type": "item","owner": "bob","text": "Cakes","created_at":"2015-06-03T14:10:38.746Z","check": false}'
+curl -X PUT http://bob:password@127.0.0.1:4984/grocery-sync/bob-pepticmeds -H "Content-Type: application/json" -d '{"type": "item","owner": "bob","text": "Peptic Meds","created_at":"2015-06-03T14:10:39.746Z","check": false}'
+curl -X PUT http://alice:password@127.0.0.1:4984/grocery-sync/bob-carrots -H "Content-Type: application/json" -d '{"type": "item","owner": "alice","text": "Carrots","created_at":"2015-06-03T14:10:40.746Z","check": false}'
+curl -X PUT http://alice:password@127.0.0.1:4984/grocery-sync/bob-greens -H "Content-Type: application/json" -d '{"type": "item","owner": "alice","text": "Greens","created_at":"2015-06-03T14:10:41.746Z","check": false}'
+curl -X PUT http://alice:password@127.0.0.1:4984/grocery-sync/bob-blueberries -H "Content-Type: application/json" -d '{"type": "item","owner": "alice","text": "Blueberries","created_at":"2015-06-03T14:10:42.746Z","check": false}'
